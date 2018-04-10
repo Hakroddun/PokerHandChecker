a798b0f2c4701f2a70857a14927f280dc716c542
Hakroddun <darkjeh@gmail.com>
2018-04-10 16:06:33 +0200

    Commit figuring out logic to deferentiate between pair and two pair.

--- src/main/java/PokerHandChecker.java
+++ src/main/java/PokerHandChecker.java
@@ -1,11 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PokerHandChecker
 {
-    public void checkHand(String card1, String card2, String card3, String card4, String card5)
+    private String hand = "High Card";
+    public void checkHand(String[] cards)
     {
+        Boolean isStraight = false;
+        Boolean isFlush = false;
+        Map numberOfCardRanks = new HashMap();
+        Map numberOfCardSuits = new HashMap();
+        for(int i=0;i<cards.length;i++)
+        {
+
+        }
     }
 
     public String BestHand()
     {
-        return "High Card";
+        return hand;
     }
 }
--- src/test/java/PokerHandTest.java
+++ src/test/java/PokerHandTest.java
@@ -1,37 +1,52 @@
-
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class PokerHandTest
 {
     private PokerHandChecker ch;
+    private String[] cards;
 
     @Before
     public void setUp()
     {
         ch = new PokerHandChecker();
+        cards = new String[5];
     }
 
     @Test
     public void canCheckPokerHand()
     {
-        ch.checkHand("","","","","");
+        cards[0]="";
+        cards[1]="";
+        cards[2]="";
+        cards[3]="";
+        cards[4]="";
+        ch.checkHand(cards);
     }
 
     @Test
     public void HighCard()
     {
-        ch.checkHand("AS","2C","3H","6C","8S");
+        cards[0]="AS";
+        cards[1]="2C";
+        cards[2]="3H";
+        cards[3]="6C";
+        cards[4]="8S";
+        ch.checkHand(cards);
         assertEquals("High Card",ch.BestHand());
     }
 
     @Test
     public void Pair()
     {
-        ch.checkHand("AS","AC","3H","6C","8S");
+        cards[0]="AS";
+        cards[1]="AC";
+        cards[2]="3H";
+        cards[3]="6C";
+        cards[4]="8S";
+        ch.checkHand(cards);
         assertEquals("Pair",ch.BestHand());
     }
 
